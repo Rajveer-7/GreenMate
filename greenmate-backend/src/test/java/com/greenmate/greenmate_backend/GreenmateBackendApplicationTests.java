7382cab965aeffc4147060f82dc35f6ff76e287c
Rajveer Singh <rajveerthisside@gmail.com>
2025-03-10 15:02:51 -0400

    Created dfferent projects for flutter and springboot backend

---
+++ greenmate-backend/src/test/java/com/greenmate/greenmate_backend/GreenmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenmate.greenmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
